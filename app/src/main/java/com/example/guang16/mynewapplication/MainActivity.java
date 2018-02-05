bbdb7aeeb3eed008e92c4f96d85a1543c1e90ac2
guang16 <guang16@uwosh.edu>
2018-02-05 15:04:16 -0600

    adding comments

--- app/src/main/java/com/example/guang16/mynewapplication/MainActivity.java
+++ app/src/main/java/com/example/guang16/mynewapplication/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.guang16.mynewapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//some changes
 public class MainActivity extends AppCompatActivity {
 
     @Override
